6e228b8c8b8542e6a1feea5042d32efedbff5685
SanchitL <sanchitxd@gmail.com>
2018-02-01 20:02:16 -0500

    class definitions imports for AddOne and its Tests

---
+++ src/com/luthra/solutions/AddOne.java
@@ -0,0 +1,7 @@
+package com.luthra.solutions;
+
+public class AddOne {
+    public static int[] addOne (int[] nums) {
+        return new int[0];
+    }
+}
---
+++ src/com/luthra/tests/AddOneTest.java
@@ -0,0 +1,8 @@
+package com.luthra.tests;
+
+import org.junit.*;
+import com.luthra.solutions.AddOne;
+
+public class AddOneTest {
+    
+}
