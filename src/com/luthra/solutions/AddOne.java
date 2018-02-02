eac709ba17a97826d19d17775f876a1a966d99d1
SanchitL <sanchitxd@gmail.com>
2018-02-02 14:44:09 -0500

    Completed addone and submission successful one github

--- src/com/luthra/solutions/AddOne.java
+++ src/com/luthra/solutions/AddOne.java
@@ -2,6 +2,31 @@ package com.luthra.solutions;
 
 public class AddOne {
     public static int[] addOne (int[] nums) {
-        return new int[0];
+        int[] result = nums.clone();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i]++;
+            if (result[i] == 10) {
+                if (i == 0) {
+                    result[0] = 0;
+                    result = extendList(result);
+                    break;
+                } else {
+                    result[i] = 0;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] extendList(int[] nums) {
+        int[] extended =new int[nums.length + 1];
+        extended[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            extended[i+1] = nums[i];
+        }
+        return extended;
     }
 }
--- src/com/luthra/tests/AddOneTest.java
+++ src/com/luthra/tests/AddOneTest.java
@@ -4,5 +4,35 @@ import org.junit.*;
 import com.luthra.solutions.AddOne;
 
 public class AddOneTest {
-    
+    @Test
+    public void TestNoCarry () {
+        int[] toAdd = {2};
+        int[] actual = AddOne.addOne(toAdd);
+        int[] expected = {3};
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestCarryNoExtend () {
+        int[] toAdd = {2, 9};
+        int[] actual = AddOne.addOne(toAdd);
+        int[] expected = {3, 0};
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestCarryAndExtend () {
+        int[] toAdd = {9};
+        int[] actual = AddOne.addOne(toAdd);
+        int[] expected = {1, 0};
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestChainedCarry () {
+        int[] toAdd = {9,9,9,9};
+        int[] actual = AddOne.addOne(toAdd);
+        int[] expected = {1,0,0,0,0};
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
