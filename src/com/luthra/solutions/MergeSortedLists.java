1b932db273b8c8d8aa67752454d577202044ed0c
SanchitL <sanchitxd@gmail.com>
2018-02-04 11:42:53 -0500

    Fixed return in MergedSortedLists. Now returns the correct list head

--- src/com/luthra/solutions/MergeSortedLists.java
+++ src/com/luthra/solutions/MergeSortedLists.java
@@ -1,14 +1,13 @@
 package com.luthra.solutions;
 
-import java.security.interfaces.RSAMultiPrimePrivateCrtKey;
-
+import java.util.ArrayList;
+import java.util.List;
 import com.luthra.util.ListNode;
 
 public class MergeSortedLists {
     public static ListNode mergeTwoSortedLists (ListNode l1, ListNode l2) {
 
-        ListNode resultHead = null;
-        ListNode resultCurrent = resultHead;
+        List<ListNode> merged = new ArrayList<>();
 
         while (l1 != null && l2 != null) {
             int toAdd;
@@ -24,29 +23,36 @@ public class MergeSortedLists {
             }
 
             if (toAdd >= 0) {
-                resultCurrent = new ListNode(toAdd);
+                merged.add(new ListNode(toAdd));
             } else {
-                resultCurrent = new ListNode(l1.val);
-                resultCurrent.next = new ListNode(l2.val);
-                resultCurrent = resultCurrent.next;
+                merged.add(new ListNode(l1.val));
+                merged.add(new ListNode(l2.val));
                 l1 = l1.next;
                 l2 = l2.next;
             }
-            resultCurrent = resultCurrent.next;
-        }
+        }        
 
         if (l2 != null) {
             while (l2 != null) {
-                resultCurrent = new ListNode(l2.val);
+                merged.add(new ListNode(l2.val));
                 l2 = l2.next;
-                resultCurrent = resultCurrent.next;
             }
         } else if (l1 != null) {
             while (l1 != null) {
-                resultCurrent = new ListNode(l1.val);
+                merged.add(new ListNode(l1.val));
                 l1 = l1.next;
-                resultCurrent = resultCurrent.next;
             }
+        }
+
+        if (merged.size() == 0)
+            return null;
+
+        ListNode resultHead = merged.get(0);
+        ListNode resultCurrent = resultHead;
+
+        for (int i = 1; i < merged.size(); i++) {
+            resultCurrent.next = merged.get(i);
+            resultCurrent = resultCurrent.next;
         }
 
         return resultHead;
--- src/com/luthra/tests/MergeSortedListsTest.java
+++ src/com/luthra/tests/MergeSortedListsTest.java
@@ -3,11 +3,19 @@ package com.luthra.tests;
 import org.junit.*;
 import com.luthra.solutions.MergeSortedLists;
 import com.luthra.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MergeSortedListsTest {
     @Test
     public void TestEqualLength () {
-        
+        int[] vals1 = {1, 3, 5};
+        int[] vals2 = {2, 4, 6};
+        int[] expected = {1,2,3,4,5,6};
+
+        ListNode result = MergeSortedLists.mergeTwoSortedLists(makeListNode(vals1), makeListNode(vals2));
+
+        Assert.assertArrayEquals(expected, makeArray(result, 6));
     }
 
     @Test
@@ -20,5 +28,34 @@ public class MergeSortedListsTest {
 
     }
 
-    public void 
+    private ListNode makeListNode (int[] vals) {
+        List<ListNode> nodes = new ArrayList<ListNode>();
+
+        for (int i : vals) {
+             nodes.add(new ListNode(i));
+        }
+
+        ListNode resultHead = nodes.get(0);
+        ListNode resultCurrent = resultHead;
+
+        for (int i = 1; i < nodes.size(); i++) {
+            resultCurrent.next = nodes.get(i);
+            resultCurrent = resultCurrent.next;
+        }
+
+        return resultHead;
+    }
+
+    private int[] makeArray (ListNode node, int size) {
+        int[] result = new int[size];
+        int i = 0;
+
+        while (node != null) {
+            result[i] = node.val;
+            node = node.next;
+            i++;
+        }
+
+        return result;
+    }
 }
